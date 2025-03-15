919447d287a6736638293039b2396a9d5cbbf09d
Mark Teper <mark.teper@gmail.com>
2025-03-14 22:23:19 -0400

    Pypi setup

--- xlnumba/__init__.py
+++ xlnumba/__init__.py
@@ -2,3 +2,5 @@ from .compiler import Compiler, get_execution_context
 from .excel_functions import xlnumba_function
 from .exceptions import *
 from .logger import logger, VERBOSE_LOG_LEVEL
+
+__version__ = '0.1.0'
